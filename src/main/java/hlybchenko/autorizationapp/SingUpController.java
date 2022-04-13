18e1606b75abe0fff25d28e786027acf95f5b2ec
Andrii <91153388+WeAreTheSameBlood@users.noreply.github.com>
2022-04-13 20:57:33 +0300

    Added code to singUp.fxml and SingUpController.

--- src/main/java/hlybchenko/autorizationapp/SingUpController.java
+++ src/main/java/hlybchenko/autorizationapp/SingUpController.java
@@ -1,4 +1,48 @@
 package hlybchenko.autorizationapp;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SingUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button singUpButton;
+
+    @FXML
+    private TextField singUpFirstName;
+
+    @FXML
+    private CheckBox singUpGenderFemale;
+
+    @FXML
+    private CheckBox singUpGenderMale;
+
+    @FXML
+    private TextField singUpLastName;
+
+    @FXML
+    private TextField singUpLocation;
+
+    @FXML
+    private TextField singUpLogin;
+
+    @FXML
+    private PasswordField singUpPassword;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
